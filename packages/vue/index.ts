b5160cc0ef2d7c3f0e38eee2fb470a386f07325f
ViPro (京京) <VdustR@gmail.com>
2022-02-15 06:44:03 +0000

    fix vue lib type

--- packages/vue/index.ts
+++ packages/vue/index.ts
@@ -1,14 +1,17 @@
 import Likftc from "@likftc/core";
-import type { ComputedRef } from "vue";
+import type { WatchSource } from "vue";
 import { watch } from "vue";
 
 export default function useLikftc<
   Source extends number | string,
   Target extends number | string = string
->(frame: ComputedRef<Source[]>, generator?: () => Target) {
-  const { get, sync } = Likftc(frame.value, generator);
-  watch(frame, () => {
-    sync(frame.value);
+>(frame: WatchSource<Source[]>, generator?: () => Target) {
+  const { get, sync } = Likftc(
+    typeof frame === "function" ? frame() : frame.value,
+    generator
+  );
+  watch(frame, (frame) => {
+    sync(frame);
   });
   return { get };
 }
